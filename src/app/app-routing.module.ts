68f2cd61b74e254aa13980bdc5ef1340e19fbc0a
Maloth Naresh <idofmalothnaresh@gmail.com>
2019-07-24 14:44:38 +0530

    Routing added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+
+import { HomeComponent } from "./home/home.component";
+
+const appRoutes: Routes = [{ path: "home", component: HomeComponent }];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(appRoutes)],
+  declarations: []
+})
+export class AppRoutingModule {}
