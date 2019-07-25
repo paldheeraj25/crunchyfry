7cab8f1001b6967470997838273fc1b8de45f7dd
user <amit.android@boltt.com>
2019-07-25 16:19:47 +0530

    added mobile menu responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from "./app-routing.module";
