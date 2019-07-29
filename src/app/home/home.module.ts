47a0cd779606f2b3b0c8439481fdb2ba998274ef
user <amit.android@boltt.com>
2019-07-29 16:25:46 +0530

    added footer

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 import { HeaderComponent } from "./../components/header/header.component";
 import { HomeComponent } from "./home.component";
 import { CrunchListComponent } from "../components/crunch-list/crunch-list.component";
+import { FooterComponent } from "../components/footer/footer.component";
 
 @NgModule({
   imports: [CommonModule,
     FormsModule
-    ],
-  declarations: [HeaderComponent,
+  ],
+  declarations: [
+    HeaderComponent,
     HomeComponent,
-    CrunchListComponent]
+    CrunchListComponent,
+    FooterComponent
+  ]
 })
 export class HomeModule { }
